707c5fd5f55c2eca3c460b64cc7cee36b3edbf64
JotaCe7 <jcisne_007@hotmail.com>
2022-10-29 15:44:21 -0500

    Add new models: resnet_v2, mobilenet, vgg16, xception

--- model/ml_service.py
+++ model/ml_service.py
@@ -5,9 +5,13 @@ import time
 import numpy as np
 import redis
 import settings
-import tensorflow
-from tensorflow.keras.applications import ResNet50
-from tensorflow.keras.applications.resnet50 import decode_predictions, preprocess_input
+# from tensorflow.keras.applications import ResNet50, ResNet101V2 
+# from tensorflow.keras.applications.resnet50 import decode_predictions, preprocess_input
+# from tensorflow.keras.applications.resnet_v2 import  preprocess_input as preprocess_inputV2
+# from tensorflow.keras.applications.resnet_v2 import  decode_predictions as decode_predictionsV2
+from tensorflow.keras.applications import resnet50, resnet_v2, mobilenet, vgg16, xception
+
+
 
 from tensorflow.keras.preprocessing import image
 
@@ -28,11 +32,24 @@ print("dssssd")
 # See https://drive.google.com/file/d/1ADuBSE4z2ZVIdn66YDSwxKv-58U7WEOn/view?usp=sharing
 # for more information about how to use this model.
 #model = ResNet50(include_top=True, weights="imagenet")
-model = ResNet50(include_top=True, weights="/src/weights/resnet50_weights_tf_dim_ordering_tf_kernels.h5")
 
 
+models= { 'ResNet50': resnet50,
+          'ResNet101V2': resnet_v2 ,
+          'MobileNet': mobilenet,
+          'VGG16': vgg16,
+          'Xception': xception}
 
-def predict(image_name):
+ResNet50 = resnet50.ResNet50(include_top=True,
+                             weights="/src/weights/resnet50_weights_tf_dim_ordering_tf_kernels.h5")
+ResNet101V2 = resnet_v2.ResNet101V2(include_top=True,
+                                    weights='/src/weights/resnet101v2_weights_tf_dim_ordering_tf_kernels.h5'
+)
+MobileNet = mobilenet.MobileNet(include_top=True,weights='imagenet')
+Xception = xception.Xception(include_top=True, weights='imagenet')
+VGG16 = vgg16.VGG16(include_top=True, weights='imagenet')
+
+def predict(image_name, NNmodel='ResNet50'):
     """
     Load image from the corresponding folder based on the image name
     received, then, run our ML model to get predictions.
@@ -48,15 +65,23 @@ def predict(image_name):
         Model predicted class as a string and the corresponding confidence
         score as a number.
     """
+    input_shape = 299 if NNmodel == 'Xception' else 224
     file_path = os.path.join(settings.UPLOAD_FOLDER,image_name)
-    img = image.load_img(file_path, target_size=(224,224))
+    img = image.load_img(file_path, target_size=(input_shape,input_shape))
     x = image.img_to_array(img)
     x_batch = np.expand_dims(x, axis=0)
 
-    x_batch = preprocess_input(x_batch)
-
+    #x_batch = preprocess_input(x_batch)
+    #preds = model.predict(x_batch)
+    #best_pred = decode_predictions(preds, top=1)
+    model = globals()[NNmodel]
+    model_class= models[NNmodel]
+    x_batch = model_class.preprocess_input(x_batch)
     preds = model.predict(x_batch)
-    best_pred = decode_predictions(preds, top=1)
+    best_pred = model_class.decode_predictions(preds, top=1)
+    
+
+    
     class_name = best_pred[0][0][1]
     pred_probability = best_pred[0][0][2]
     # TODO
@@ -92,7 +117,7 @@ def classify_process():
         queue_name, job_data_str = db.brpop(settings.REDIS_QUEUE)
         job_data = json.loads(job_data_str)
 
-        class_name, pred_probability = predict(job_data['image_name'])
+        class_name, pred_probability = predict(job_data['image_name'], 'Xception')
         pred_dict = {
                       "prediction": class_name,
                       "score": float(pred_probability),
