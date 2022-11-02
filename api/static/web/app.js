b065a5a2a2545caef6de752bdde2ce3bd786ec3a
JotaCe7 <jcisne_007@hotmail.com>
2022-11-02 08:56:37 -0500

    modify style.css

--- api/static/web/app.js
+++ api/static/web/app.js
@@ -1,19 +1,21 @@
 // alert("Hello world!")
-var greeting = "Hello comander";
-// document.getElementById("title").innerHTML = greeting;
-document.getElementById("mybtn").onclick = function() {myFunction()};
-function myFunction() {
-  document.getElementById("paragraph").innerHTML = greeting;
-}
+// var greeting = "Hello comander";
+// // document.getElementById("title").innerHTML = greeting;
+// document.getElementById("mybtn").onclick = function() {myFunction()};
+// function myFunction() {
+//   document.getElementById("paragraph").innerHTML = greeting;
+// }
 
-const image_input = document.querySelector("#image_input");
+
+
+const image_input = document.querySelector("#image_input2");
 var uploaded_image = ""
 
 image_input.addEventListener("change", function() {
   const reader = new FileReader();
   reader.addEventListener("load", () => {
     uploaded_image = reader.result;
-    document.querySelector("#display_image").style.backgroundImage = `url(${uploaded_image})`;
+    document.querySelector("#display_image2").style.backgroundImage = `url(${uploaded_image})`;
   })
   reader.readAsDataURL(this.files[0]);
   // TODO: delete console.log
